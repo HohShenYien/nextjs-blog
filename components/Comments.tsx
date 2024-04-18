58d13259d6529256056a96bbbaf45523d94604be
HohShenYien <hohshenyien@gmail.com>
2024-04-19 00:46:22 +0800

    Updated Comments component

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,18 +1,9 @@
 'use client'
 
 import { Comments as CommentsComponent } from 'pliny/comments'
-import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
+import React from 'react'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(true)
-  return (
-    <>
-      {loadComments ? (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      ) : (
-        <button onClick={() => setLoadComments(true)}>Load Comments</button>
-      )}
-    </>
-  )
+  return <CommentsComponent commentsConfig={siteMetadata.comments!} slug={slug} />
 }
